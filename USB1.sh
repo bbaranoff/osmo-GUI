ddbbb22fdc645cd6c8adce9f716ef415ebd0fa7f
nirvana <nirvana@kali>
2020-02-13 18:54:16 +0100

    commit

--- USB1.sh
+++ USB1.sh
@@ -3,4 +3,4 @@ set -x
 set -e
 
 byobu new-window -t root:1 -n 'USB1'
-byobu send-keys -t USB1 "cd /root/osmo-GUI && ./osmoUSB0.sh" C-m
+byobu send-keys -t USB1 "cd /root/osmo-GUI && ./osmoUSB1.sh" C-m
