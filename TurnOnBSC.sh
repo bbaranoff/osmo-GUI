e9979eda43b7528edfbc08bae03193a0ea74d206
nirvana <nirvana@kali>
2020-02-13 17:57:53 +0100

    commit

--- TurnOnBSC.sh
+++ TurnOnBSC.sh
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 /root/osmo-GUI/osmo-nitb -c ~/.osmocom/open-bsc.cfg -P -m -C --debug=DRLL:DCC:DMM:DRR:DRSL:DNM
-=======
-#!/usr/bin/env bash
-exec "bash -c \"osmo-nitb -c ~/.osmocom/open-bsc.cfg -l ~/.osmocom/hlr.sqlite3 -P -m -C --debug=DRLL:DCC:DMM:DRR:DRSL:DNM;"
->>>>>>> a2c5b3f45748b8c280ce73823f3ac7aa08b41b02
+
